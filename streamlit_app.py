2c5f5edd1c52530f83786938baad795b5523fcd1
jxiaofirn <148923934+jxiaofirn@users.noreply.github.com>
2023-11-02 11:02:36 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page
 sl.dataframe(fruits_to_show)
 
-# get_fruityvice_data f(x)
+# get_fruityvice_data()
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
@@ -45,6 +45,23 @@ except URLError as e:
   
 sl.write('The user entered ', fruit_choice)
 
+# snowflake fruit load list
+sl.header("The fruit load list contains:")
+
+# get_fruit_load_list()
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+
+# button to load fruit
+if sl.button('Get Fruit Load List'):
+  my_cnx = sfc.connect(**sl.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  sl.dataframe(my_data_rows)
+
+
+
 sl.stop()
 
 # establish connection to snowflake
@@ -53,7 +70,7 @@ my_cur = my_cnx.cursor()
 
 # get fruit_load_list and output
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-sl.text("The fruit load list contains:")
+sl.header("The fruit load list contains:")
 my_data_row = my_cur.fetchone()
 
 # allow user to add a fruit to list
